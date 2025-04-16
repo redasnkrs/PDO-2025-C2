ea1b87ba2addd9dfa4810cf73f44c2355ecba027
WebDevCF2m <gitweb@cf2m.be>
2025-04-16 14:21:56 +0200

    13 ok

--- formateur/13-exe-prepa-TI2-insert-select/view/homepage.view.php
+++ formateur/13-exe-prepa-TI2-insert-select/view/homepage.view.php
@@ -11,9 +11,22 @@
 <body>
 <h1>Exercice</h1>
 <h2>Laissez-nous un message</h2>
+<?php
+// si on a inséré un article
+if(isset($thanks)):
+?>
+<h3 class="thanks"><?=$thanks?></h3>
+<?php
+
+elseif(isset($error)):
+?>
+    <h4 class="error"><?=$error?></h4>
+<?php
+endif;
+?>
 <form action="" method="post">
     <label for="name">Nom</label>
-    <input type="text" name="name" id="name" required>
+    <input type="text" name="surname" id="name" required>
     <label for="email">Email</label>
     <input type="email" name="email" id="email" required>
     <label for="message">Message</label>
@@ -21,23 +34,39 @@
     <button type="submit">Envoyer</button>
 </form>
 <hr>
-<pre>
-Affichage du nombre de messages :
-
-Pas de message : Pas encore de message
-1 message : Il y a 1 message
-Plusieurs messages : Il y a X messages
-</pre>
-<hr>
-<pre>
-    Nos messages par date DESC
-
-    NOM
-    TEXTE
-    DATE
-</pre>
 <?php
-var_dump($_POST);
+// articles est un tableau vide
+if(empty($articles)):
+?>
+<div class="nomessage">
+<h3>Pas encore d'article</h3>
+    <?php
+// nous avons au moins un article
+else:
+    // on peut compter le nombre d'articles
+    $countArticle = count($articles);
+    // ternaire pour ajouter un s à article
+    // si on en a plus d'un
+    $pluriel = $countArticle>1? "s" : "";
+    ?>
+<div class="messages">
+<h3>Nous avons <?=$countArticle?> article<?=$pluriel?></h3>
+<hr>
+
+    <?php
+    // tant qu'on a des articles
+    foreach($articles as $article):
+    ?>
+<h4><?=$article['surname']?></h4>
+        <p><?=nl2br($article['message']);// retour à la ligne automatique?></p>
+        <h5><?=$article['create_date']?></h5><hr>
+<?php
+    endforeach;
+endif;
+?>
+</div>
+<?php
+#var_dump($_POST,$articles,$countArticle);
 ?>
 <hr>
 </body>
